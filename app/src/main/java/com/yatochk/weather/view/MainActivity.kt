e5777157fd6407a6c2631575d884d087cf9c337f
yatochk <yatochk@github.com>
2018-10-30 02:13:01 +0400

    first commit

---
+++ app/src/main/java/com/yatochk/weather/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yatochk.weather.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.yatochk.weather.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
